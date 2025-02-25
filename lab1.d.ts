69c861626e869df7af39d5eac3b4c10bd82a4e52
exhaustion666 <crowford-1337@mail.ru>
2025-02-25 19:28:27 +0400

    lab1

---
+++ lab1.d.ts
@@ -0,0 +1,16 @@
+declare function Alert(): void;
+declare const sum: (a: number, b: number, c: number) => number;
+declare const result: number;
+declare const firstName = "Nikita";
+declare let lastName: string;
+declare const middleName = "Vyacheslavovich";
+declare const passport: string[];
+declare let age: number;
+declare const allData: (string | number | string[])[];
+declare const data: {
+    id: number;
+    name: string;
+    surname: string;
+};
+declare const jsonStringify: string;
+declare const someObject: any;
